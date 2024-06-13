24f40b72e714c2a3c3128e60b1e868107369a029
FunctionalGeneralist <jeffrey@functionalgeneralist.com>
2024-06-13 06:17:07 -0700

    Added https

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Sitewide configuration things.
-export const serverAddress = "https://98.177.34.8:80/"
-export const infoEndpoint = "info"
+export const serverAddress = "https://98.177.34.8:80"
+export const infoEndpoint = "/info"
